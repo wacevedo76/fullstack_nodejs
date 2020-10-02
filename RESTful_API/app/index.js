e0fec074be672b325b861db39ca73a1246d0e757
William Acevedo <william.acevedo@gmail.com>
2020-10-02 12:16:22 +0200

    added more to api, ended on headers section of api

--- RESTful_API/app/index.js
+++ RESTful_API/app/index.js
@@ -1,1 +1,40 @@
-console.log('Hello world')
+/*
+ * Primary file for the API
+ *
+ */
+
+const http = require('http');
+const url = require('url');
+
+// The server should respod to all request with a string
+
+const server = http.createServer(function(req, res) {
+  // get the url and parse it
+  let parseURL = url.parse(req.url, true);
+
+  // get the path
+  let path = parseURL.pathname;
+  let trimmedPath = path.replace(/^\/+|\/+$/g,'')
+
+  // Get the query string as an objec
+  let queryStringObject = parseURL.query;
+
+  //  Get the HTTP Method
+  let method = req.method.toLowerCase();
+
+  // Get the HTTP headers as an object
+  let headers = req.headers;
+                               
+  // Send the response
+  res.end('Hello world\n');
+
+  // log the request path
+  console.log('the headers are', headers);
+  
+})
+
+// Start the server, and have it listen on port 3000
+server.listen(3000, () => {
+  console.log("The server is listening on port 3000 now")
+});
+
