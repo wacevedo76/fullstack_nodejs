5cd395faa2859543a652ad0c81998754c13de6ab
William Acevedo <william.acevedo@gmail.com>
2020-10-03 01:36:14 +0200

    completes parse payload section

--- RESTful_API/app/index.js
+++ RESTful_API/app/index.js
@@ -1,10 +1,11 @@
-/*
+/* 
  * Primary file for the API
  *
  */
 
 const http = require('http');
 const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
 
 // The server should respod to all request with a string
 
@@ -16,7 +17,7 @@ const server = http.createServer(function(req, res) {
   let path = parseURL.pathname;
   let trimmedPath = path.replace(/^\/+|\/+$/g,'')
 
-  // Get the query string as an objec
+  // Get the query string as an object
   let queryStringObject = parseURL.query;
 
   //  Get the HTTP Method
@@ -25,13 +26,23 @@ const server = http.createServer(function(req, res) {
   // Get the HTTP headers as an object
   let headers = req.headers;
                                
-  // Send the response
-  res.end('Hello world\n');
-
-  // log the request path
-  console.log('the headers are', headers);
+  // Get the payload, if any
+  let decoder = new StringDecoder('utf-8');
+  var buffer = '';
+  req.on('data', function(data) {
+    buffer += decoder.write(data);
+  });
   
-})
+  req.on('end', function() {
+    buffer += decoder.end();
+
+    // Send the response
+    res.end('Hello world\n');
+
+    // log the request path
+    console.log('The payload is: ', buffer);
+  });
+});
 
 // Start the server, and have it listen on port 3000
 server.listen(3000, () => {
