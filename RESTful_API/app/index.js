c82269196ec58bcbfcb2bdffff61f346e7f7e028
William Acevedo <william.acevedo@gmail.com>
2020-10-05 23:51:06 +0200

    adds json Content-type

--- RESTful_API/app/index.js
+++ RESTful_API/app/index.js
@@ -61,6 +61,7 @@ const server = http.createServer(function(req, res) {
       const payloadString = JSON.stringify(payload);
 
       // return the response
+      res.setHeader('Content-Type', 'application/json');
       res.writeHead(statusCode);
       res.end(payloadString);
  
