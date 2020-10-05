3d9fd180bcc0a7580961af3878c03ce0889be3ee
William Acevedo <william.acevedo@gmail.com>
2020-10-05 23:28:14 +0200

    adds router object, and routing handlers.  encapsulates data gathered in begining of server repl cycle to create a data object to send to the respective handler

--- RESTful_API/app/index.js
+++ RESTful_API/app/index.js
@@ -10,15 +10,16 @@ const StringDecoder = require('string_decoder').StringDecoder;
 // The server should respod to all request with a string
 
 const server = http.createServer(function(req, res) {
+  // get data needed for RESTful request, create dataobject with all needed data
   // get the url and parse it
-  let parseURL = url.parse(req.url, true);
+  let parsedURL = url.parse(req.url, true);
 
   // get the path
-  let path = parseURL.pathname;
+  let path = parsedURL.pathname;
   let trimmedPath = path.replace(/^\/+|\/+$/g,'')
 
   // Get the query string as an object
-  let queryStringObject = parseURL.query;
+  let queryStringObject = parsedURL.query;
 
   //  Get the HTTP Method
   let method = req.method.toLowerCase();
@@ -36,16 +37,60 @@ const server = http.createServer(function(req, res) {
   req.on('end', function() {
     buffer += decoder.end();
 
-    // Send the response
-    res.end('Hello world\n');
+    // Construct the data object to go to the handler
+    const data  = {
+      'trimmedPath' : trimmedPath,
+      'queryStringObject' : queryStringObject,
+      'method' : method,
+      'headers' : headers,
+      'payload' : buffer
+    };
 
-    // log the request path
-    console.log('The payload is: ', buffer);
+    // Choose the handler this request should go to. If non found, choose 'not found'
+    let chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+
+    // Route the request to the handler specified in the router
+    chosenHandler(data, function(statusCode, payload){
+      // Use the status code called back by the hander, or default to 200
+      statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
+      
+      // Use the payload called by the handler, or default to an empty object
+      payload = typeof(payload) == 'object' ? payload : {};
+
+      // Conver teh payload to a string
+      const payloadString = JSON.stringify(payload);
+
+      // return the response
+      res.writeHead(statusCode);
+      res.end(payloadString);
+ 
+      // log the request path
+      console.log("returning the response: ", statusCode, payloadString);
+    });
+
   });
 });
 
 // Start the server, and have it listen on port 3000
 server.listen(3000, () => {
-  console.log("The server is listening on port 3000 now")
+  console.log('Listing on port 3000');
 });
 
+// Define the handlers
+const handlers = {};
+
+// Sample hander
+handlers.sample = function(data, callback) {
+  // Callback a http status code, and a payload object
+  callback(406, {'name' : 'sample handler'} );
+};
+
+// Not found handler
+handlers.notFound = function(data, callback) {
+  callback(404);
+};
+
+// Define a request router
+const router = {
+  'sample' : handlers.sample
+};
