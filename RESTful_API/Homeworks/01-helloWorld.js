9d8d6caaad72afa2863e4a320c997a746b87cab8
William Acevedo <william.acevedo@gmail.com>
2020-10-07 23:38:03 +0200

    continued work on Homework, stripped extrainious code

--- RESTful_API/Homeworks/01-helloWorld.js
+++ RESTful_API/Homeworks/01-helloWorld.js
@@ -3,32 +3,21 @@
  * Homework Assignment 1 - Hello World
  *
  */
+
 const http = require('http');
 const url = require('url');
 
-// instantiate server
-const server = http.createServer(function(req, res) {
-  // parse and trim url
+const helloData = {
+  'Hello' : 'World'
+};
+
+const server = http.createServer(function(req, res){
+  // Parse url
   const parsedURL = url.parse(req.url, true);
-  // get path
-  const path = parsedURL.pathname;
-  const trimmedPath = path.replace(/^\/+|\/+$/g, '');
-  
+  // return response to browser
+  res.end(parsedURL);
 });
 
-server.listen(3000, function(){
-  console.log('server started on 3000')
+server.listen(3000, function() {
+  console.log('listening on port 3000');
 });
-
-const handlers = {};
-handlers.home = function(data, callback) {
-  callback(200);
-};
-
-handlers.notFound = function(data, callback) {
-  callback(404);
-};
-
-const router = {
-  'hello' : handlers.hello
-};
