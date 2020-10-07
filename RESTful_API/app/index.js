83b9fc2581a7aad5d69a444a2a470296e1ffb374
William Acevedo <william.acevedo@gmail.com>
2020-10-07 16:34:44 +0200

    adds ping route

--- RESTful_API/app/index.js
+++ RESTful_API/app/index.js
@@ -103,10 +103,8 @@ const unifiedServer = function(req,res) {
 // Define the handlers
 const handlers = {};
 
-// Sample hander
-handlers.sample = function(data, callback) {
-  // Callback a http status code, and a payload object
-  callback(406, {'name' : 'sample handler'} );
+handlers.ping =  function(data, callback) {
+  callback(200);
 };
 
 // Not found handler
@@ -116,5 +114,5 @@ handlers.notFound = function(data, callback) {
 
 // Define a request router
 const router = {
-  'sample' : handlers.sample
+  'ping' : handlers.ping
 };
