5dc719ab825d589192b81962ab524065d9e0c711
William Acevedo <william.acevedo@gmail.com>
2020-10-09 08:32:31 +0200

    completes homwork, submitted and accepted

--- RESTful_API/Homeworks/01-helloWorld.js
+++ RESTful_API/Homeworks/01-helloWorld.js
@@ -1,21 +1,28 @@
 /*
- *
  * Homework Assignment 1 - Hello World
- *
  */
 
 const http = require('http');
 const url = require('url');
 
-const helloData = {
-  'Hello' : 'World'
-};
-
 const server = http.createServer(function(req, res){
   // Parse url
   const parsedURL = url.parse(req.url, true);
-  // return response to browser
-  res.end(parsedURL);
+  // get path
+  const path = parsedURL.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+  // Get the query string as an object
+
+  if (trimmedPath == 'hello') {
+    
+    res.setHeader('Content-Type', 'applicatiion/json');
+    res.writeHead(200);
+    res.end(JSON.stringify( {message: 'Hello World!'} ));
+  } else {
+    res.setHeader('Content-Type', 'plain/text');
+    res.writeHead(404);
+    res.end('Content not found.')
+  }
 });
 
 server.listen(3000, function() {
