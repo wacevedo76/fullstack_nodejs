36b5601a2b5938e3d8c24ddacf8a1aaa84898a65
William Acevedo <william.acevedo@gmail.com>
2020-10-21 14:11:26 +0200

    Clean up parseJsonToObject helper and created return random string helper

--- RESTful_API/lib/helpers.js
+++ RESTful_API/lib/helpers.js
@@ -12,7 +12,7 @@ const helpers = {};
 // Create a SHA256 hash
 helpers.hash = function(str) {
   if(typeof(str) == 'string' && str.length > 0) {
-    const hash = crypto.createHmac('sha256', config.hashingSecret).update(str).digest('hex');
+    const hash = crypto.cre config.hashingSecret).update(str).digest('hex');
     return hash;
   } else {
     console.log('could not hash password');
@@ -21,14 +21,36 @@ helpers.hash = function(str) {
 };
 
 // Parse a JSON string to an object in all cases, without throwing 
-  helpers.parseJsonToObject = function(str){
-    try{
-      let obj = JSON.parse(str);
-      return obj;
-    }catch(e){
-      return {};
-    }
-  };
+helpers.parseJsonToObject = function(str){
+  try{
+    let obj = JSON.parse(str);
+    return obj;
+  }catch(e){
+    return {};
+  }
+};
+
+// Create a string of random alphanumeric characters of a given length
+helpers.createRandomString = function(strLength){
+  strLength = typeof(strLength) == 'number' && strLength > 0 ? strLength : false;
+  if(strLength){
+    // Define all the possible characters that could go into a string
+    const possibleCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789[](){}<>?!:;*&^%$#@';
+    
+    let str = '';
+    for(let i = 1; i <= strLength; i++){
+      // Get a random character from the possible characters
+      const randomCharacter = possibleCharacters.charAt(Math.floor(Math.random() * possibleCharacters.length));
+
+      // Append this character to the final string
+      str+=randomCharacter;
+    };
+    // Return string
+    return str;
+  } else {
+    return false;
+  }
+};
 
 // Export modules
 module.exports = helpers;
