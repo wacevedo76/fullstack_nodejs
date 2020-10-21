3c48105b512806ae0c05f67d74e42b561f025be3
William Acevedo <william.acevedo@gmail.com>
2020-10-21 14:09:25 +0200

    Clean up lib/config

--- RESTful_API/lib/config.js
+++ RESTful_API/lib/config.js
@@ -1,6 +1,5 @@
 /*
  * Create and export configuration variables
- *
  */
 
 // Container for all the environments
@@ -22,14 +21,11 @@ environments.production = {
   'hashingSecret' : 'thisIsAlsoASecret'
 };
 
-
 // Determine which environment was passed as a command-line argument
-
 const currentEnvironment = typeof(process.env.NODE_ENV) == 'string' ? process.env.NODE_ENV.toLowerCase() : ''; 
 
 // Check that the current environment is one of the environments above, if not, default to staging
 const environmentToExport = typeof(environments[currentEnvironment]) == 'object' ? environments[currentEnvironment] : environments.staging;
 
 // Export the module
-
 module.exports = environmentToExport;
