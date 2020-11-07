8836c9b47e64df66eca39518ad016b3ad87f14ba
William Acevedo <william.acevedo@gmail.com>
2020-11-07 00:45:28 -0500

    Completes Twilio API connection

--- RESTful_API/lib/helpers.js
+++ RESTful_API/lib/helpers.js
@@ -5,6 +5,8 @@
 // Dependencies
 const crypto = require('crypto');
 const config = require('../lib/config');
+const https = require('https');
+const querystring = require('querystring');
 
 // Helpers containers
 const helpers = {};
@@ -54,3 +56,60 @@ helpers.createRandomString = function(strLength){
 
 // Export modules
 module.exports = helpers;
+
+// Send SMS message via Twilio
+helpers.sendTwilioSms = function(phone,msg,callback){
+  // Validate parameters
+  phone = typeof(phone) == 'string' && phone.trim().length == 10 ? phone.trim() : false;
+  msg = typeof(msg) == 'string' && msg.trim().length > 0 && msg.trim(). length <= 1600 ? msg.trim() : false;
+  if(phone && msg){
+    // Configure the request payload
+     const payload = {
+       'From' : config.twilio.fromPhone,
+       'To' : '+1'+phone,
+       'Body' : msg
+     };
+
+    // Stringify the payload
+    const stringPayload = querystring.stringify(payload);
+    
+    // Configure the request details
+    const requestDetails = {
+      'protocol' : 'https:',
+      'hostname' : 'api.twilio.com',
+      'method' : 'POST',
+      'path' : '/2010-04-01/Accounts/'+config.twilio.accoutSid+'Messages.json',
+      'auth' : config.twilio.accoutSid+':'+config.twilio.authToken,
+      'headers' : {
+        'Content-Type' : 'application/x-www-form-urlencoded',
+        'Content-Length' : Buffer.byteLength(stringPayload)
+      }
+    };
+
+    // Instantiat the request object
+    const req = https.request(requestDetails,function(res){
+      // Grab the status of the sent request
+      const status = res.statusCode;
+      // Callback successfully if the request went through
+      if(status == 200 || status == 201){
+        callback(false);
+      } else {
+        callback('Status code returned was '+status)
+      }
+    });
+
+    // bind to the error event so it doesn't get thrown' 
+    req.on('error',function(e){
+      callback(e);
+    });
+
+    // Add the payload
+    req.write(stringPayload);
+
+    // End the request
+    req.end();
+
+  } else {
+    callback('Given parameters were missing or invalid');
+  }
+};
